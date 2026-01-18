93382df715ef35e473a984b2085448b17af36e82
stanleymauricio <stanleymauricio.sm@gmail.com>
2026-01-18 17:04:54 -0500

    feat: dynamic product list

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,3 +1,10 @@
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  const htmlStrings = list.map(templateFn);
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
