5e099d57dbec37e9a3739ace27bcc6e0f4cb5e93
stanleymauricio <stanleymauricio.sm@gmail.com>
2026-01-18 16:09:01 -0500

    add

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,15 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  // 1. Obtener lo que ya hay en el carrito. Si está vacío, usar un arreglo []
-  const cartItems = getLocalStorage("so-cart") || [];
+const productId = getParam('product');
 
-  // 2. Agregar el nuevo producto al arreglo
-  cartItems.push(product);
 
-  // 3. Guardar el arreglo completo de nuevo en localStorage
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const dataSource = new ProductData('tents');
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+
+const product = new ProductDetails(productId, dataSource);
+
+
+product.init();
