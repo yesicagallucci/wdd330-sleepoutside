921191880d2c9c3afcc33346ec629c6121517465
clarkedav <clarkdav282@gmail.com>
2026-01-22 17:23:52 +0100

    dynamic product

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,10 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
+const productId = getParam('product'); // gets ?product=880RR
+const dataSource = new ProductData('tents'); // loads tents.json
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init(); // fetch data, render, and set up add-to-cart
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
